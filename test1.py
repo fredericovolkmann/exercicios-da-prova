09c2cbc23e5691b26da0b5a3d70cb2e5e0ec0627
frederico volkmann <fredericovolkmann19@gmail.com>
2024-12-13 22:57:58 -0300

    test1.py

--- test1.py
+++ test1.py
@@ -1,29 +1,26 @@
-Y=input("digite uma palavra em letra maiuscula:")
-lista = [Y]
+Y = input("digite uma palavra em letra maiuscula:")
+x = list(Y)
 
-x = [list(palavra) for palavra in lista]
+A = 0
+E = 0
+I = 0
+O = 0
+U = 0
 
-for i in range(len(x)-1,-1,-1):
- 
- A=0
- E=0
- I=0
- O=0
- U=0
- if i==A:
-    A+=1
- elif i==E:
-    E+=1
- elif i==I:
-    I+=1
- elif i==O:
-    O+=1
- elif i==U:
-    U+=1
- 
-print(A)
-print(E)
-print(I)
-print(O)
-print(U)
- 
+for letra in x:
+    if letra == 'A':
+        A += 1
+    elif letra == 'E':
+        E += 1
+    elif letra == 'I':
+        I += 1
+    elif letra == 'O':
+        O += 1
+    elif letra == 'U':
+        U += 1
+
+print("A:", A)
+print("E:", E)
+print("E:", E)
+print("O:", O)
+print("U:", U)
